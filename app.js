a8bcd0bc381c22a08f869a319816c02abbdc0d18
Maxim <supreme@107.su>
2018-03-13 16:26:38 +0300

    up

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var cheerio = require("cheerio");
 var async = require("async");
 
 var aUrl = [];
-aUrl[0] = "https://tmall.aliexpress.com/item/Lenovo-320-15ISK-15-6-i3-6006U-500-4-G920MX-noODD-Win10/1000005036795.html?spm=a2g02.8538954.p79498a.1.6a7cfd13dbb6m2";
-aUrl[1] = "https://tmall.aliexpress.com/item/HP-15-bw083ur-15-6-A9-9420-500-6-AMD-R520-DVD-RW-Win10/1000005035871.html?spm=a2g0r.10010208.1000013.5.1162cf6faax1be&traffic_analysisId=recommend_2088_3_-1_iswistore&scm=1007.13339.90158.0&pvid=22c1d615-40cb-4e6c-ba20-1b4c21562f9b&tpp=1";
-aUrl[2] = "https://tmall.aliexpress.com/item/Apple-MacBook-Pro-13-2-3-Dual-Core-i5-128/1000004612394.html?spm=a2g02.8538954.p79498a.2.6a7cfd13dbb6m2";
+aUrl[0] = "http://lesdomsad.ru/shop/sad/gazonokosilki-kupit-v-kirove/benzinovye/huskvarna/10383.html";
+aUrl[1] = "http://lesdomsad.ru/shop/sad/gazonokosilki-kupit-v-kirove/benzinovye/huskvarna/10386.html";
+aUrl[2] = "http://lesdomsad.ru/shop/sad/gazonokosilki-kupit-v-kirove/benzinovye/huskvarna/11076.html";
 
 
 var q = async.queue(function(url){
@@ -14,12 +14,12 @@ var q = async.queue(function(url){
 
         var $ = cheerio.load(res.body);
 
-        console.log($(".product-name").text());
+        console.log($("h1").text());
 
-        img = $(".img-thumb-item img");
+        img = $(".image").find("img");
 
         img.each(function(i,val){
-            console.log($(val).attr("src").replace("50x50","640x640"));
+            console.log($(val).attr("src"));
         });
 
     });
